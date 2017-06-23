17c9d07e589cb1e4653d5323a5fe26049a0306af
speedyco <rvdijssel@live.nl>
2017-06-23 12:15:53 +0200

    Update myapi.js
    default content, change on implementation

--- myapi.js
+++ myapi.js
@@ -1,1 +1,28 @@
-.
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+var inputs = [{ pin: '11', gpio: '17', value: 1 },
+              { pin: '12', gpio: '18', value: 0 }];
+
+  app.use(express['static'](__dirname ));
+
+// Express route for incoming requests for a customer name
+app.get('/inputs/:id', function(req, res) {
+  res.status(200).send(inputs[req.params.id]);
+}); 
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function(req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+});
